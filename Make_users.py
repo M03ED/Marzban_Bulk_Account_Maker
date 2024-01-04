82f4dfa618b1c3f86dd5ed05d3b7dc0f19245f8b
Random Guy <50927468+M03ED@users.noreply.github.com>
2024-01-04 19:51:09 +0330

    Make on hold users

--- Make_users.py
+++ Make_users.py
@@ -3,20 +3,34 @@ import datetime
 import logging
 
 
-API_URL = 'https://Domain.com:port/api'  # Replace with the actual API URL
-BASE_NAME = 'user'  # Base name for the new users
-START_NUMBER = 1  # Start number for the new users
-NUM_USERS = 5  # Number of new users to create
-DATA_LIMIT_GB = 15  # Data limit per GB
-Days = 12 # Expire Date per Day
 Username = "user"
 Password = "pass"
+Api_url = 'https://domain.com:1234/api'  # Replace with the actual API URL
+Base_name = 'user'  # Base name for the new users
+Start_number = 22  # Start number for the new users
+Num_users = 10  # Number of new users to create
+Data_limit_GB = 15  # Data limit per GB
+Days = 12 # Expire Date per Day (enter 0 for unlimite)
+flow = 'xtls-rprx-vision'
+status = 'active' # replace with on_hold for count after first connection
+data_limit_reset_strategy = 'no_reset'
+
+proxies = {
+    'vmess': {},
+    'vless': {
+        'flow': flow
+        }
+}
+inbounds = {
+    'vmess': ['VMESS_TCP_INBOUND'],
+    'vless': ['VLESS_TCP_Reality_INBOUND', 'VLESS_TCP_INBOUND']
+}
 
 def generate_username(base_name, number):
     return f"{base_name}{number}"
 
 def get_access_token(username, password):
-    url = f"{API_URL}/admin/token"
+    url = f"{Api_url}/admin/token"
     data = {
         'username': username,
         'password': password
@@ -31,20 +45,38 @@ def get_access_token(username, password):
         logging.error(f'Error occurred while obtaining access token: {e}')
         return None
 
-def create_new_user(access_token, username, proxies, inbounds, expire, data_limit, data_limit_reset_strategy):
-    url = f"{API_URL}/user"
+def create_user_payload(username, proxies, inbounds, expire, data_limit):
+    if status == 'active':
+        payload = {
+            'username': username,
+            'proxies': proxies,
+            'inbounds': inbounds,
+            'expire': expire,
+            'status' : status,
+            'data_limit': data_limit,
+            'data_limit_reset_strategy': data_limit_reset_strategy
+        }
+    elif status == 'on_hold':
+        payload = {
+            'username': username,
+            'proxies': proxies,
+            'inbounds': inbounds,
+            'on_hold_expire_duration': expire,
+            'status' : status,
+            'data_limit': data_limit,
+            'data_limit_reset_strategy': data_limit_reset_strategy
+        }
+    else:
+        print("wrong status.")
+
+    return payload
+
+def create_new_user(access_token, username, payload):
+    url = f"{Api_url}/user"
     headers = {
         'accept': 'application/json',
         'Content-Type': 'application/json',
         'Authorization': f"Bearer {access_token}"
-    }
-    payload = {
-        'username': username,
-        'proxies': proxies,
-        'inbounds': inbounds,
-        'expire': expire,
-        'data_limit': data_limit,
-        'data_limit_reset_strategy': data_limit_reset_strategy
     }
 
     try:
@@ -57,31 +89,35 @@ def create_new_user(access_token, username, proxies, inbounds, expire, data_limi
 
 logging.basicConfig(filename='script_log.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def generate_users(base_name, start_number, num_users, data_limit):
-    data_limit_bytes = data_limit * 1024**3  # Convert data limit from GB to bytes
 
-    access_token = get_access_token(Username, Password)  # Replace with your actual username and password
+def generate_users(base_name, start_number, Num_users, data_limit):
+    data_limit_bytes = data_limit * 1024 ** 3
+
+    access_token = get_access_token(Username, Password)
 
     if access_token:
-        for i in range(start_number, start_number + num_users):
-            username = generate_username(base_name, i)
-            proxies = {
-                'vmess': {},
-                'vless': {
-                    'flow': 'xtls-rprx-vision'
-                    }
-            }
-            inbounds = {
-                'vmess': ['VMESS_TCP_INBOUND'],
-                'vless': ['VLESS_TCP_Reality_INBOUND', 'VLESS_TCP_INBOUND']
-            }
-            expire = int(datetime.datetime.now().timestamp()) + (24 * 3600 * (Days+1))  # Expiring in 7 days
 
-            user = create_new_user(access_token, username, proxies, inbounds, expire, data_limit_bytes, 'no_reset')
+        for i in range(start_number, start_number + Num_users):
+            username = generate_username(base_name, i)
+            
+            if status == 'active':
+                if Days == 0:
+                    expire = None
+                else:
+                    expire = int(datetime.datetime.now().timestamp()) + \
+                        (24 * 3600 * (Days + 1))
+            else:
+                expire = (24 * 3600 * Days)
+            
+            payload = create_user_payload(username, proxies, inbounds, expire, data_limit_bytes)
+            if payload :
+                user = create_new_user(access_token, username, payload)
             if user:
-                print(f"User {username} created successfully.")
+                subscription_url = user.get('subscription_url', '')
+                if subscription_url:
+                    print(f"{username} sub link: {subscription_url}")
     else:
         print("Failed to obtain the access token.")
 
 # Generate new users
-generate_users(BASE_NAME, START_NUMBER, NUM_USERS, DATA_LIMIT_GB)
+generate_users(Base_name, Start_number, Num_users, Data_limit_GB)
--- README.md
+++ README.md
@@ -3,28 +3,33 @@
 Change Variables Depend On Your Usage
 
 ```python
-API_URL = 'https://Domain.com:port/api'  # Replace with the actual API URL
-BASE_NAME = 'user'  # Base name for the new users
-START_NUMBER = 1  # Start number for the new users
-NUM_USERS = 5  # Number of new users to create
-DATA_LIMIT_GB = 15  # Data limit per GB
-Days = 12 # Expire Date per Day
 Username = "user"
 Password = "pass"
+Api_url = 'https://domain.com:1234/api'  # Replace with the actual API URL
+Base_name = 'user'  # Base name for the new users
+Start_number = 22  # Start number for the new users
+Num_users = 10  # Number of new users to create
+Data_limit_GB = 15  # Data limit per GB
+Days = 12 # Expire Date per Day (enter 0 for unlimite)
+flow = 'xtls-rprx-vision'
+status = 'active' # replace with on_hold for count after first connection
+data_limit_reset_strategy = 'no_reset'
 ```
 Change proxies and inbounds depend on your marzban setting , you can add trojan if you want
 ```python
 proxies = {
-                'vmess': {},
-                'vless': {
-                    'flow': 'xtls-rprx-vision'
-                    }
-            }
-            inbounds = {
-                'vmess': ['VMESS_TCP_INBOUND'],
-                'vless': ['VLESS_TCP_Reality_INBOUND', 'VLESS_TCP_INBOUND']
-            }
+    'vmess': {},
+    'vless': {
+        'flow': flow
+        }
+}
+inbounds = {
+    'vmess': ['VMESS_TCP_INBOUND'],
+    'vless': ['VLESS_TCP_Reality_INBOUND', 'VLESS_TCP_INBOUND']
+}
 ```
 After you start script it will start making users with usernames like these
 - user1 , user2 , user3 , user4 , user5
 - `START_NUMBER` is which number you wanna start and `NUM_USERS` its number of users you wanna create
+- if you need to make on hold users replace status with 'on_hold'
+- if you need to make users with unlimited expire replace days with 0
